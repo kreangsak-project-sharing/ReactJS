810be27afeea7aeba8af290484863756d674f6ac
Kreangsak <128896272+kreangsak-project-sharing@users.noreply.github.com>
2023-08-16 21:54:37 +0700

    Update Layout.jsx

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { Box, Container, useTheme } from "@mui/material";
+import { Box, Container } from "@mui/material";
 
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
 
 const Layout = () => {
-  const theme = useTheme();
 
   return (
-    <Box sx={{ background: theme.colors.background.default }} height="100vh">
+    <Box>
       <ResponsiveAppBar />
       <Container maxWidth="xl">
         <Outlet />
